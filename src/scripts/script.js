4df6ad04eab01a181792620739e008c98f3e20b0
Justin <12guilhrm12@gmail.com>
2025-10-29 08:49:32 -0300

    New Background

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,22 @@
+// Mobile Device \\
+
 $(document).ready(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-bars fa-xmark');
+    });
+});
+
+// Navbar Hover \\
+
+var navItems = document.querySelectorAll('.navbar-item');
+
+navItems.forEach(function(navItem) {
+    navItem.addEventListener('mouseenter', function() {
+        navItem.classList.add('active');
+    });
+
+    navItem.addEventListener('mouseleave', function() {
+        navItem.classList.remove('active');
     });
 });
